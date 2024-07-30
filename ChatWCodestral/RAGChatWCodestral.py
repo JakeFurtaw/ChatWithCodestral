2f6a1f2dd011c26429b0fbc6a7e9e521827177c1
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-07-30 11:18:18 -0400

    cleaned up main by adding new functions

--- ChatWCodestral/RAGChatWCodestral.py
+++ ChatWCodestral/RAGChatWCodestral.py
@@ -2,25 +2,47 @@ from llama_index.llms.ollama import Ollama
 from llama_index.core import SimpleDirectoryReader
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from ChatWYourRepo.utils import setup_index_and_chat_engine
+import os
+
+DIRECTORY_PATH = ""
 
 
-def main() -> None:
-    # Setting up LLM and Embedding models
-    llm = Ollama(model="codestral:latest",
-                 request_timeout=30.0,
-                 device="cuda:0")
+def set_embed_model():
     embed_model = HuggingFaceEmbedding(
         model_name="dunzhang/stella_en_1.5B_v5",
         device="cuda:1"
     )
-    # Reading and creating doc dictionary
-    reader = SimpleDirectoryReader(input_dir="INSERT DIRECTORY HERE", recursive=True).load_data()
-    all_docs = []
-    for docs in reader:
-        for doc in docs:
-            all_docs.append(doc)
+    return embed_model
+
+
+def set_llm():
+    llm = Ollama(model="codestral:latest",
+                 request_timeout=30.0,
+                 device="cuda:0")
+    return llm
+
+
+def has_multiple_files(directory):
+    file_count = sum(1 for item in os.listdir(directory) if os.path.isfile(os.path.join(directory, item)))
+    return file_count > 1
+
+
+def load_docs():
+    directory_path = DIRECTORY_PATH
+    if has_multiple_files(directory_path):
+        reader = SimpleDirectoryReader(input_dir=directory_path, recursive=True).load_data()
+        documents = []
+        for docs in reader:
+            for doc in docs:
+                documents.append(doc)
+    else:
+        documents = SimpleDirectoryReader(input_dir=directory_path).load_data()
+    return documents
+
+
+def main(embed_model, documents, llm) -> None:
     # Setting up chat engine
-    chat_engine = setup_index_and_chat_engine(docs=all_docs, llm=llm, embed_model=embed_model)
+    chat_engine = setup_index_and_chat_engine(docs=documents, llm=llm, embed_model=embed_model)
     # Looping chat until user is done chatting
     while (query := input("Enter your coding question(e to exit): ")) != "e":
         response = chat_engine.chat(query)
